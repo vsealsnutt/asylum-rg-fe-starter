53c3922531fa52f9210a00e946a00353e905dee6
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-06-08 11:43:28 -0400

    added a login control component for login/logout buttons

---
+++ src/components/Auth/LoginControl.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import LoginButton from './loginComponent';
+import LogoutButton from './logoutComponent';
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return <div>{button}</div>;
+  }
+}
+
+export default LoginControl;
