8f66b30e389886cab5521fbbbe06d7a967705a59
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-06-08 15:50:28 -0400

    cleaned up code in Graph Wrapper

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -73,8 +73,6 @@ function GraphWrapper(props) {
     
     */
 
-    let url = 'https://hrf-asylum-be-b.herokuapp.com/cases';
-
     let summary = '';
     let formatData;
 
@@ -93,7 +91,7 @@ function GraphWrapper(props) {
     }
 
     axios
-      .get(`${url}/${summary}`, {
+      .get(`${process.env.REACT_APP_API_URI}/${summary}`, {
         params: {
           from: years[0],
           to: years[1],
@@ -101,93 +99,11 @@ function GraphWrapper(props) {
         },
       })
       .then(result => {
-        stateSettingCallback(view, office, formatData(result.data)); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+        stateSettingCallback(view, office, formatData(result.data));
       })
       .catch(err => {
         console.error(err);
       });
-
-    // if (office === 'all' || !office) {
-    //   switch (view) {
-    //     case 'time-series':
-    //     case 'office-heat-map':
-    //       axios
-    //         .get(`${url}/fiscalSummary`, {
-    //       // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-    //           params: {
-    //             from: years[0],
-    //             to: years[1],
-    //           },
-    //         })
-    //         .then(result => {
-    //           stateSettingCallback(view, office, [result.data]); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-    //         })
-    //         .catch(err => {
-    //           console.error(err);
-    //         });
-    //       break;
-    //     case 'citizenship':
-    //       axios
-    //         .get(`${url}/citizenshipSummary`, {
-    //     // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-    //           params: {
-    //             from: years[0],
-    //             to: years[1],
-    //           },
-    //         })
-    //         .then(result => {
-    //           console.log('testing', result.data, result.data?.data);
-    //           stateSettingCallback(view, office, formatData); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-    //         })
-    //         .catch(err => {
-    //           console.error(err);
-    //         });
-    //       break;
-    //     default:
-    //       break;
-    //   }
-    // } else {
-    //   switch (view) {
-    //     case 'time-series':
-    //       axios
-    //         .get(`${url}/fiscalSummary`, {
-    //           // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-    //           params: {
-    //             from: years[0],
-    //             to: years[1],
-    //             office: office,
-    //           },
-    //         })
-    //         .then(result => {
-    //           console.log(result);
-    //           stateSettingCallback(view, office, [result.data]); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-    //         })
-    //         .catch(err => {
-    //           console.error(err);
-    //         });
-    //         break;
-    //       case 'citizenship':
-    //         axios
-    //           .get(`${url}/citizenshipSummary`, {
-    //           // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-    //           params: {
-    //             from: years[0],
-    //             to: years[1],
-    //             office: office,
-    //           },
-    //         })
-    //         .then(result => {
-    //           console.log(result);
-    //           stateSettingCallback(view, office, result.data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-    //         })
-    //         .catch(err => {
-    //           console.error(err);
-    //         });
-    //         break;
-    //       default:
-    //         break;
-    //     }
-    // }
   }
   const clearQuery = (view, office) => {
     dispatch(resetVisualizationQuery(view, office));
